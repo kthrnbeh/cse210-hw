4e98301ef70e68127e2ec7e2391a93b35eb4693d
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-16 16:38:10 -0600

    prep2 update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,32 +7,34 @@ class Program
         Console.WriteLine("Hello Prep2 World!");
         //The program awards the letter grade for User as they put in there score.
 
-    {
-        Console.Write("Please enter your test score:");
-        string score = Console.ReadLine();
-        int  letter = int.Parse(score);
-        if (letter >= 90) // I made two mistakes here first I used numbers to  compare instead of letter
         {
-            Console.Write("A"); //second I didn't remember how to comment and used # instead of //
-        }
-        else if (letter >= 80) // I inquired the web with how to comment in c+
-        {
-            Console.Write("B"); //Then asked chatGPT to teach me what I did wrong on the first if statement.
-        }
-        else if (letter >= 70)
-        {
-            Console.Write("C");
-        }
-        else if (letter >= 60)
-        {
-            Console.Write("D");
-        }
-        else
-        {
-            Console.Write("F"); // I also saw I used score instead of the word letter.I updated that.
-        }
-        Console.WriteLine($"Your Letter Grade is {letter}."); //Prints letter grade.
-            
+            Console.Write("Please enter your test score:");
+            string score = Console.ReadLine();
+            int letter = int.Parse(score);
+            string grade = " ";
+            if (letter >= 90) // I made two mistakes here first I used numbers to  compare instead of letter
+            {
+                grade = "A"; //second I didn't remember how to comment and used # instead of //
+            }
+            else if (letter >= 80) // I inquired the web with how to comment in c+
+            {
+                grade = "B"; //Then asked chatGPT to teach me what I did wrong on the first if statement.
+            }
+            else if (letter >= 70)
+            {
+                grade = "C";
+            }
+            else if (letter >= 60)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F"; // I also saw I used score instead of the word letter.I updated that.
+            }
+            Console.WriteLine($"Your Letter Grade is {grade}."); //Prints letter grade.
+                                                                 // my statement kept printing the score  not grade. I had to look up what I did wrong. I asked to be taught what to do. 
+                                                                // After I was like "Oh yeah I learned that in python... I just forgot." 
         }
         
     }
