6a86f29761c1b9bab03a6256941101fb699e75da
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-16 16:07:33 -0600

    Lettergrade

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -12,3 +12,40 @@ class Program
         Console.WriteLine($"Your name is {lastName},{firstName}{lastName}.");
     }
 }
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+# The program awards the letter grade for User as they put in there score.
+
+    {
+        Console.Write("Please enter your test score:");
+        string score = Console.ReadLine();
+        int scoreAsInt = int.Parse(score);
+        if (90 > 80)
+        {
+            Console.Write("A");
+        }
+        else if (80 > 70)
+        {
+            Console.Write("B");
+        }
+        else if (70 > 60)
+        {
+            Console.Write("C");
+        }
+        else if (60 > 50)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("F");
+        }
+        Console.WriteLine($"Your Letter Grade is {scoreAsInt}.");
+            
+        }
+    }
+
+}
