4dd4b0d29bf349c10362c0f29989d8ee14172653
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-16 16:17:50 -0600

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        //The program awards the letter grade for User as they put in there score.
+
+    {
+        Console.Write("Please enter your test score:");
+        string score = Console.ReadLine();
+        int scoreAsInt = int.Parse(score);
+        if (scoreAsInt > 90)
+        {
+            Console.Write("A");
+        }
+        else if (scoreAsInt > 80)
+        {
+            Console.Write("B");
+        }
+        else if (scoreAsInt > 70)
+        {
+            Console.Write("C");
+        }
+        else if (scoreAsInt > 60)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("F");
+        }
+        Console.WriteLine($"Your Letter Grade is {scoreAsInt}."); //Prints letter grade.
+            
+        }
+        
     }
 }
