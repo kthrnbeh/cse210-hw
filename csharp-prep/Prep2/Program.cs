5f97b218329638960de212cada2558a57db3dcf9
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-16 16:31:49 -0600

    Update prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,20 +10,20 @@ class Program
     {
         Console.Write("Please enter your test score:");
         string score = Console.ReadLine();
-        int letter = int.Parse(score);
-        if (letter > 90) // I made two mistakes here first I used numbers to  compare instead of letter
+        int  letter = int.Parse(score);
+        if (letter >= 90) // I made two mistakes here first I used numbers to  compare instead of letter
         {
             Console.Write("A"); //second I didn't remember how to comment and used # instead of //
         }
-        else if (80 > letter) // I inquired the web with how to comment in c+
+        else if (letter >= 80) // I inquired the web with how to comment in c+
         {
             Console.Write("B"); //Then asked chatGPT to teach me what I did wrong on the first if statement.
         }
-        else if (70 > letter)
+        else if (letter >= 70)
         {
             Console.Write("C");
         }
-        else if (60 > letter)
+        else if (letter >= 60)
         {
             Console.Write("D");
         }
