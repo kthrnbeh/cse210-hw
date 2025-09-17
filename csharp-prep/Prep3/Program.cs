4ff05025dfd87cd5ef4ba970db9ccfd0fec28635
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-17 10:53:35 -0600

    I tried its hard

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,8 +6,8 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
         //Prep 3 Guess My Number
-        //Console.Write("What is the magic number?");
-        //int magicNumber = int.Parse(Console.ReadLine()); //Part 1 ask user what the magic number is
+        Console.Write("What is the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine()); //Part 1 ask user what the magic number is
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101); //Part three random number generator.
         int guessCount = 0; //stretch keep track of the users number of guesses.
@@ -16,8 +16,19 @@ class Program
         guessCount++; //Stretch count the first guess.
         string response;     //Stretch task two ask user if they want to play again.
 
-        
-        do (userGuess != magicNumber) // Part two add while look. //Updated from user guess to random
+    do
+    {
+            //asked Chat why my code wasn't working and it explained to put it inside do, 
+            //I agreed because your proram showed to put the needed code inside the while
+        string response;     //Stretch task two ask user if they want to play again.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); //Part three random number generator.
+        int guessCount = 0; //stretch keep track of the users number of guesses.
+        Console.Write("What is your guess?");
+        int userGuess = int.Parse(Console.ReadLine());
+        guessCount++; //Stretch count the first guess.
+    
+         while (userGuess != magicNumber) // Part two add while look. //Updated from user guess to random
         {
             if (userGuess < magicNumber) // Part 1 ask until get the proper answer. //updated to random 
             {
@@ -33,11 +44,12 @@ class Program
             userGuess = int.Parse(Console.ReadLine());
         }
         Console.WriteLine("You guessed it!");
-                 guessCount++; //Stretch count the final guess.
+        guessCount++; //Stretch count the final guess.
         Console.WriteLine($"It took you {guessCount} guesses."); //Stretch print the number of guesses.
-         Console.Write("Do you want to continue? ");
-    response = Console.ReadLine();
-    
-
+        Console.Write("Do you want to continue? ");
+        response = Console.ReadLine();
     } while (response == "yes");
+        
+
+    } 
 }
