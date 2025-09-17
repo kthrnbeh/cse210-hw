d9a71acff0eb54a0519ed6da2d4add91e121f675
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-17 10:13:51 -0600

    Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        //Prep 3 Guess My Number
+        Console.Write("What is the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine()); //Part 1 ask user what the magic number is
+        Console.Write("What is your guess?");
+        int userGuess = int.Parse(Console.ReadLine());//Part 2 ask user what their guess is.
+        while (userGuess != magicNumber) // Part two add while look. 
+        {
+            if (userGuess < magicNumber) //Make if statement to see if guess correct or needs to be higher or lower.
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess?");
+            userGuess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("You guesssed it!");
     }
 }
