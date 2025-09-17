753b1a87e932fb2c9e26c4a91d659ec5c0897254
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-17 10:22:01 -0600

    update part 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,13 +6,15 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
         //Prep 3 Guess My Number
-        Console.Write("What is the magic number?");
-        int magicNumber = int.Parse(Console.ReadLine()); //Part 1 ask user what the magic number is
+        //Console.Write("What is the magic number?");
+        //int magicNumber = int.Parse(Console.ReadLine()); //Part 1 ask user what the magic number is
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100); //Part three random number generator.
         Console.Write("What is your guess?");
         int userGuess = int.Parse(Console.ReadLine());//Part 2 ask user what their guess is.
-        while (userGuess != magicNumber) // Part two add while look. 
+        while (userGuess != magicNumber) // Part two add while look. //Updated from user guess to random
         {
-            if (userGuess < magicNumber) // Part 1 ask until get the proper answer.
+            if (userGuess < magicNumber) // Part 1 ask until get the proper answer. //updated to random 
             {
                 Console.WriteLine("Higher");
             }
