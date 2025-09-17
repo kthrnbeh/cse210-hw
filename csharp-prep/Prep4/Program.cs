d453f4745513ef3e67ae469a1943a87dda5d2af3
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-17 11:46:33 -0600

    update numbers list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,15 +8,22 @@ class Program
         // For this prep 4 Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
         // ask user for a number 
         Console.Write("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+        int userInput = int.Parse(Console.ReadLine()); // For a sec I forgot to add this. Good thing I checked the assignment.
         //Create list to hold numbers 
         List<int> numbers = new List<int>();
-       
+        //Now I need to append a series of numbesrs to the list until they enter 0.
         //Compute the sum, or total, of the numbers in the list.
         int sum = 0;
         //Compute the average of the numbers in the list.
         float average = 0.0f;
         //Find the maximum, or largest, number in the list.
         int max = int.List<int>.Max();
+        //Create while Loop and then get results to all the above.
+        while (userInput != 0)
+        {
+            numbers.Add(userInput); //append new numbers to list from user
+        }
     }
 
 }
