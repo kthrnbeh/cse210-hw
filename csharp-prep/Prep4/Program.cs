2373f9527bf179ec5e2afd3d66740c22180932d5
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-17 13:41:14 -0600

    update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,19 +8,15 @@ class Program
         Console.WriteLine("Hello prep4 "); //To remind me this is prep 4 
         //I am restarting. This is very difficult for me for some reason.
         //Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
-        Console.WriteLine("Hello User, Enter a list of numbers, type 0 when finished.")
+        Console.WriteLine("Hello User, Enter a list of numbers, type 0 when finished.");
         //Once you have a list, have your program do the following:
         List<int> numbers = new List<int>();
-        //Compute the sum, or total, of the numbers in the list.
-        total sum = list.sum();
-        //Compute the average of the numbers in the list.
-        average = sum / list.count();
-        //Find the maximum number in the list.
-        max = list.max();
-        do
+        int userNumber = -1; // The user won't put a negative number so it can keep going until 0
+        while (userNumber !=0) //while the user number is not 0 the user will keep inputting
         {
             Console.Write("Enter a number:");
             int number = int.Parse(Console.ReadLine());
+            numbers.Add(number); //We need to add the numbers to the list. 
         }
 
 
