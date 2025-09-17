3f28949c4e35369aa314eb124ab6f5574dfeb54c
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-17 11:39:30 -0600

    ok starting

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        // For this prep 4 Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+        // ask user for a number 
+        Console.Write("Enter a list of numbers, type 0 when finished.");
+        //Create list to hold numbers 
+        List<int> numbers = new List<int>();
+       
+        //Compute the sum, or total, of the numbers in the list.
+        int sum = 0;
+        //Compute the average of the numbers in the list.
+        float average = 0.0f;
+        //Find the maximum, or largest, number in the list.
+        int max = int.List<int>.Max();
     }
+
 }
