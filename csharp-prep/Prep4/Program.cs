78c72cf7d4dcc5172cf2a3e61e2616aebb702e03
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-18 09:51:17 -0600

    update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -44,7 +44,7 @@ class Program
             
            
                     
-                }
+            
                 Console.WriteLine($"The sum is: {sum}");
                 Console.WriteLine($"The average is: {average}"); //I had to cast sum to a float to get the decimal answer.
                 Console.WriteLine($"The Largest number is: {Largest}"); //finding tips from the example
