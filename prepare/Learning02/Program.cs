f4eefd3f091e5e04c2ae70f5e7fdd51051ececed
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-23 09:36:58 -0600

    step 1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,3 +7,18 @@ class Program
         Console.WriteLine("Hello Learning02 World!");
     }
 }
+public class Job
+{
+    _name: string;
+    _jobs: List<Job>;
+}
+public class Resume
+{
+    _company: string;
+    _jobtitle: string;
+    _startyear; int;
+    _endyear: int;
+    Display(): void
+    
+
+}
