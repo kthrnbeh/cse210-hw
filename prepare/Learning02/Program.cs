036e55196c5d0bcdc5208b46707a05fe2b149bba
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-23 10:09:46 -0600

    Update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,5 +1,35 @@
 using System;
+using System.Collections.Generic;
 
+
+//Following the instructions from the Abstraction overview //I am trying to create a workin thing hope this is right.
+class Job
+{
+    //Step 3 ? Should these be moved up here? 
+    public string Company;
+    public string Jobtitle;
+    public int Startyear;
+    public int Endyear;
+    public string Name;
+    public List<Job> Jobs;
+
+    //Step 1: You program should contain two classes one for a Job and one for the Resume itself
+    // this is step 3 actually making it public. 
+    public class Job(string company, string jobTitle, int startYear, int endYear) // the second word should be capalized oh yeah. 
+    {
+        Company = company;
+        Jobtitle = jobTitle;
+        Startyear = startYear;
+        Endyear = endYear;
+    }
+    // Ugh thi
+    public class Resume(string name, List<Job> jobs)
+    {
+        Name = name;
+        Jobs = jobs;
+     }
+        
+}
 class Program
 {
     static void Main(string[] args)
@@ -8,20 +38,6 @@ class Program
         job1._jobTitle = "Software Engineer";
     }
 }
-//Step 1: You program should contain two classes one for a Job and one for the Resume itself
-public class Job
-{
-    _name: string;
-    _jobs: List<Job>;
-    Display(): void
-}
-public class Resume
-{
-    _company: string;
-    _jobtitle: string;
-    _startyear; int;
-    _endyear: int;
-    Display(): void
-    
+
 
 }
