0645f984ed190b81c4fdc5a9029c2ab30cb6250e
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-24 11:29:23 -0600

    updates

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        // Working with my team to develope a journal prompt 
+        //https://www.youtube.com/watch?v=3bBiQ1G7BOU Watched this to help me with ideas of what to do.
+        //Looks like first thing I need to do is make a menu
+        // I thought about what a menu would look like in python as they can do similar things but in a different method
+        //ok I need to make a couple classes , Entry and Jornal
+
+
+    }
+    public class Entry
+    {
+
+    }
+    public class Journal
+    {
+        
     }
 }
