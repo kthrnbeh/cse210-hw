d11eb25560a3b299c5ce2ee38b180963cec65d95
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-24 15:11:23 -0600

    delete the if

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -12,26 +12,28 @@ class Program
         // I thought about what a menu would look like in python as they can do similar things but in a different method
         //ok I need to make a couple classes , Entry and Jornal
         //Need 
-        _string prompt;
-        _string quit;
-        _string write;
-        _int menuNumber;
-        _string load;
-        _string save;
+        string prompt;
+        string quit;
+        string write; // looked at reading and it  said this was the right way
+                         // activity prep 2 and I am trying to figure it out with my teacm
+        string load;
+        string save;
 
         int menuNumber = -1;
-        while (menuNumber != 5) ;
+        while (menuNumber != 5) 
         {
             //my idea is to write a menu and have it keep going until it gets the number 5
             Console.WriteLine("Please select one of the following choices:");
-            if (menuNumber == 1) ;
-            {
-                Console.WriteLine("1. Write");
-                Console.WriteLine("2. Display");
-                Console.WriteLine("3. Load");
-                Console.WriteLine("4. Save");
-                Console.WriteLine("5. Quit");
-            }
+            //By removing the if this should make the program always give the menu
+            
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would your like to do?");
+            int userAnswer = Console.ReadLine(int.Parse(userAnswer));
+            
         }    
 
     }
