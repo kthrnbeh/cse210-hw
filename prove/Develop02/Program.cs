bc38e3ce37163ea661dc0c4661dfb351b9f3997a
kthrnbeh <kthrnbeh@yahoo.com>
2025-09-24 11:53:33 -0600

    updates

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -12,17 +12,22 @@ class Program
         // I thought about what a menu would look like in python as they can do similar things but in a different method
         //ok I need to make a couple classes , Entry and Jornal
         int menunumber = -1;
-        while (menunumber > 0) ;
-        {
-            Console.WriteLine("Please select one of the following choices:");
-            if (menunumber == 1) ;
-            
-        }
+        
 
     }
     public class Entry
     {
         static void prompt(string self, string words, int num);
+        while (menunumber > 0) ;
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            if (menunumber == 1) ;
+            {
+                Console.WriteLine("1.Write");
+                
+             }
+            
+        }
 
     }
     public class Journal
