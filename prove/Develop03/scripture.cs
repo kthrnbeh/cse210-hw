60f0540c0617b761b0f5aca6c8f2b2660aa3d43a
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-01 13:07:46 -0600

    update

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 public class Scripture
 {
     private _reference();
-    private_words(List);
-    public hiddingWords()
+    private List<Words>_words; // referencing my two other files 
+    public Scripture
+    
+    public Scripture (Scripture script, string FullWord)
     {
-
-    }
-
-    public getDisplay()
-    {
-
+         _reference = script;
+        _words = new List<Words>();
+         foreach (var piece in FullWord.Split(' '))
+         _words.Add(new Words(piece)); // breaks the scripture into words every space
     }
     public completelyHidden()
     {
@@ -22,5 +22,10 @@ public class Scripture
 }
 
 
-
-P
+//  public Passage(Citation cite, string fullText)
+//    {
+      //  _citation = cite;
+      //  _tokens = new List<Token>();
+      //  foreach (var piece in fullText.Split(' '))
+       //     _tokens.Add(new Token(piece));
+//    }
