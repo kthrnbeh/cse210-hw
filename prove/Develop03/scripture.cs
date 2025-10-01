d804228d2fd4e6f113d88b1a946a300924593787
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-01 15:42:21 -0600

    update

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -13,9 +13,29 @@ public class Scripture
          foreach (var piece in FullWord.Split(' '))
          _words.Add(new Words(piece)); // breaks the scripture into words every space
     }
-    public completelyHidden()
+    public string getDisplay()// oh now I get it I want to take the words and lines and put themtogether into the list.
     {
+        var parts = new List<string>();
+        foreach (var word in _words) parts.Add(word.View());
+        return _refence.Label() + "\n" + string.Join("" , parts);
+    }
+    
 
+   
+
+  
+    public HideDisplay()
+
+    {
+    foreach (var word in _words)
+        if (!word.IsMasked())
+        {
+            return false:
+        }
+        else
+        {
+        return true;
+        }
     }
 
 
