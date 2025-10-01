99a3d9f97550925f9daf6d146133176f1dcca9d4
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-01 16:22:09 -0600

    update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 using System.IO; //reads files into an ary of strings and prints them. writting to not forget what this does.
 
 //team scripture code
@@ -10,5 +10,14 @@ class Program // calling all the things
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        // list from the scripture.cs 
+        Scripture script = new List<Scripture>();
+        if (!File.Exists(path))
+        {
+            return script;
+        }
     }
+
+
 }
+//https://byui-cse.github.io/cse210-course-2023/unit01/csharp-5.html
