41f43374a15479fba6d3ca6d486026e66c059aa9
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-02 10:03:59 -0600

    oh i did it twice

--- prove/Develop03/reference.cs
+++ prove/Develop03/reference.cs
@@ -3,7 +3,6 @@ public class Reference
 {
 
     private string _book; // was instructed that I shouldn't have ()
-    private string _book;
     private int _chapter;
     private int _startVerse;
     private int _endVerse;
