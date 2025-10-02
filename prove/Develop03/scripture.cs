1310893090ede0120ffdeeb5d178fc8024266893
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-01 18:53:47 -0600

    fix

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -5,31 +5,41 @@ public class Scripture
     private Reference _reference;
     private List<Word> _words; // referencing my two other files 
 
-    public Scripture(Reference reference, string FullWord) //mixed up which program
+    public Scripture(Reference reference, string fullText) //mixed up which program
     {
         _reference = script;
         _words = new List<Word>();
-        string[] pieces = fullText.Split('';)
-        foreach (var piece in FullWord.Split(' '))
-            _words.Add(new Words(piece)); // breaks the scripture into words every space
+        string[] pieces = fullText.Split('');// ah now it makes sense why it wasn't working.
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            if (pieces[i].Length > 0)
+            {
+                _words.Add(new Word(pieces[i]));
+            }
+        }// breaks the scripture into words every space
     }
     public string getDisplay()// oh now I get it I want to take the words and lines and put themtogether into the list.
     {
         var parts = new List<string>();
-        foreach (var word in _words) parts.Add(word.Present());
-        return _reference.Label() + "\n" + string.Join("", parts);
+        foreach (var word in _words)
+            parts.Add(word.Present());
+
+        return _reference.GetDisplayText() + "\n" + string.Join("", parts);
     }
     public void HideRandomWords(int count, Random rng)// grabs the word sets it to true
-
     {
-        for (int i = 0; i < count; i++)
+        if (_words.Count == 0)
         {
-            int index = rng.Next(0, _words.Count);
-            _words[index].Hide();//loop funs how many times it counts
+            return;
         }
+        for (int i = 0; i < count; i++)
+            {
+                int index = rng.Next(0, _words.Count);
+                _words[index].Hide();//loop funs how many times it counts
+            }
         public bool IsFullyHidden()//check to see if scripture is fully gone
     {
-        for (int i = 0; i < _words.Count; i++)
+      for (int i = 0; i < _words.Count; i++)
         {
             if (!_words[i].IsHidden())
             {
