e9e72ca4ab947b4684377296f13c0f7ee0ac6eb3
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-06 14:18:42 -0600

    steps

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -8,8 +8,12 @@ class Program
         //Verify that you can run the project. Did.
         //Test your class by returning to the Main method in the Program.cs file. 
         // Create a simple assignment, call the method to get the summary, and then display it to the screen.
-        
+
         Assignment assignment = new Assignment("Gary, Steven", "History");
         Console.WriteLine(assignment.GetSummary());
+        MathAssignment math = new MathAssignment("GaryBob","Fraction", "8.3"," 8-19");
+        Console.WriteLine(math.GetSummary());
+        WritingAssignment write = new WritingAssignment("Mary Waters","European Histroy","The Causes of World war 2 by Mary Waters.");
+        Console.WriteLine(write.GetSummary());
     }
 }
