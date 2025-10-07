0b3e690b9ff3231d6f2ba81a796387ab5c46ffd3
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-07 13:52:29 -0600

    update

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,46 +1,39 @@
 public class BreathingActivity : Activity
 {
 
-    public BreathingActivity():base()
+    public BreathingActivity() : base()
     {
         _activity = "Breathing Activity";
-        _description = "";
+        _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
 
     }
-    public string BreathingCycle(); //name didn't really relate
+    public void BreathingCycle(); //name didn't really relate //void not returning anything
     { 
-         StartMessageDisplay();
-        DateTime startTime = DateTime.Now;
-        DateTime stopTime = startTime.AddSeconds(_time);
-        while(DateTime.Now<stopTime) {
+             StartMessageDisplay();
+            DateTime startTime = DateTime.Now;
+            DateTime stopTime = startTime.AddSeconds(_time);
+            while(DateTime.Now=<stopTime) 
+            {
             //While loop to tell it when to stop
          //have user breath in for 5 seconds
-         Console.Write("Breathe In");
-          CountDown(5);
-         // have user breath out for 5 seconds
-        // repeat(loop)
-        // Console.WriteLine("Breath In")
-       
-        Console.WriteLine("Breathout")
-        CountDown(5);
-        EndMessageDisplay();
-                string s = animationstrings(i);
-            Console.Write(s);
-            Thread.Speed(1000);
-            Console.Write("\b\b");
-            if (i>=animationstrings.count)
-            {
-                i=0;
-            }
+             Console.Write("Breathe In");
+            CountDown(5);
+            // have user breath out for 5 seconds
+            // repeat(loop)
+            // Console.WriteLine("Breath In")
+
+             Console.WriteLine("Breathout")
+                CountDown(5);
+               
         }
-    
+             EndMessageDisplay();
 
-
+    }
     //While loop to tell it when to stop
 
     //have user breath in for 5 seconds
     // have user breath out for 5 seconds
     // repeat(loop)
-        
+
 }
 //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives
