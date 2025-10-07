14b5912cef1d1bb76fbae84057df223e00876dd7
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-07 13:53:37 -0600

    fixes

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -4,36 +4,34 @@ public class BreathingActivity : Activity
     public BreathingActivity() : base()
     {
         _activity = "Breathing Activity";
-        _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
-
+        _description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
     }
-    public void BreathingCycle(); //name didn't really relate //void not returning anything
+
+    public void BreathingCycle() // void not returning anything
     { 
-             StartMessageDisplay();
-            DateTime startTime = DateTime.Now;
-            DateTime stopTime = startTime.AddSeconds(_time);
-            while(DateTime.Now=<stopTime) 
-            {
+        StartMessageDisplay();
+        DateTime startTime = DateTime.Now;
+        DateTime stopTime = startTime.AddSeconds(_time);
+        
+        while (DateTime.Now <= stopTime)
+        {
             //While loop to tell it when to stop
-         //have user breath in for 5 seconds
-             Console.Write("Breathe In");
+            //have user breathe in for 5 seconds
+            Console.Write("Breathe In");
             CountDown(5);
-            // have user breath out for 5 seconds
-            // repeat(loop)
-            // Console.WriteLine("Breath In")
-
-             Console.WriteLine("Breathout")
-                CountDown(5);
-               
+            
+            // have user breathe out for 5 seconds
+            Console.WriteLine("Breathe Out");
+            CountDown(5);
         }
-             EndMessageDisplay();
 
+        EndMessageDisplay();
     }
+
     //While loop to tell it when to stop
-
-    //have user breath in for 5 seconds
-    // have user breath out for 5 seconds
+    //have user breathe in for 5 seconds
+    // have user breathe out for 5 seconds
     // repeat(loop)
-
 }
+
 //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives
