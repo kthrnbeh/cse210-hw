46f81662cdc74bbe25f8d3d90d0662be7edbc31a
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-07 13:43:54 -0600

    fix order

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,19 +1,30 @@
 public class BreathingActivity : Activity
 {
 
-    public BreathingActivity()
+    public BreathingActivity():base()
     {
         _activity = "Breathing Activity";
         _description = "";
 
     }
-    public string RandomPrompt();
-    {
-        StartMessageDisplay();
-    DateTime startTime = DateTime.Now;
-    DateTime stopTime = startTime.AddSeconds(_time);
+    public string BreathingCycle(); //name didn't really relate
+    { 
+         StartMessageDisplay();
+        DateTime startTime = DateTime.Now;
+        DateTime stopTime = startTime.AddSeconds(_time);
         while(DateTime.Now<stopTime) {
-            string s = animationstrings(i);
+            //While loop to tell it when to stop
+         //have user breath in for 5 seconds
+         Console.Write("Breathe In");
+          CountDown(5);
+         // have user breath out for 5 seconds
+        // repeat(loop)
+        // Console.WriteLine("Breath In")
+       
+        Console.WriteLine("Breathout")
+        CountDown(5);
+        EndMessageDisplay();
+                string s = animationstrings(i);
             Console.Write(s);
             Thread.Speed(1000);
             Console.Write("\b\b");
@@ -24,15 +35,12 @@ public class BreathingActivity : Activity
         }
     
 
+
     //While loop to tell it when to stop
 
     //have user breath in for 5 seconds
     // have user breath out for 5 seconds
     // repeat(loop)
-        Console.WriteLine("Breath In")
-        CountDown(5);
-        Console.WriteLine("Breathout")
-        CountDown(5);
-        EndMessageDisplay();
-    }
+        
 }
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives
