2b9c0a740f933c6f964da0490f3a4aec36fcfa35
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-07 14:20:10 -0600

    updates

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -3,11 +3,24 @@ public class ReflecionActivity : Activity
     private List<string> _prompts = new List<string>()
     {
         // Add prompts here.
+        Think of a time when you stood up for someone else.
+        Think of a time when you did something really difficult.
+        Think of a time when you helped someone in need.
+        Think of a time when you did something truly selfless.
     };
 
     private List<string> _questions = new List<string>()
     {
         // Add questions here.
+        Why was this experience meaningful to you?
+        Have you ever done anything like this before?
+        How did you get started?
+        How did you feel when it was complete?
+        What made this time different than other times when you were not as successful?
+        What is your favorite thing about this experience?
+        What could you learn from this experience that applies to other situations?
+        What did you learn about yourself through this experience?
+    How can you keep this experience in mind in the future?
     };
 
     public ReflecionActivity()
