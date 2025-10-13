444c14bf9ebf68538e28e8af05afd04a472bfd41
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-13 13:38:03 -0600

    updates

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,7 +1,13 @@
 public class Entry
 {
-    public Entry()
+    public string _prompt;
+    public int _time;
+    public string _response;
+    public Entry(string prompt, int time, string response)
     {
+        _prompt = prompt;
+        _time = time;
+        _response = response;
         
     }
 }
