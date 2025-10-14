091e854e697b50a224f60c7258dbd46d75ca56f8
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-14 12:42:55 -0600

    update

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,8 +1,18 @@
-public abstract class Shape // first part
+public abstract class Shape // base class for all shapes
 {
-    private string _color; //diagram
+    private string _color; // from diagram (_color : string)
+
     public string GetColor()
     {
-        
+        // TODO: return the color
     }
+
+    public void SetColor()
+    {
+        // TODO: set the color value (parameter should be a string)
+    }
+
+    public abstract double GetArea(); 
+    // TODO: override this method in derived classes (Square, Rectangle, Circle)
+    // purpose: calculate and return the area for each shape type
 }
