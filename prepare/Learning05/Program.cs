88321b84fb12ecb5af92049453ea5e7f26d82556
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-14 13:15:41 -0600

    ok

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -10,11 +10,11 @@ class Program
         // TODO: create a list to hold shapes (List<Shape>)
         // example: List<Shape> shapes = new List<Shape>();
         List<Shape> shapes = new List<Shape>();
-        Square square = new Square();
+        Square square = new Square("Blue", .9);
         shapes.Add(square);
-        Rectangle rect = new Rectangle();
+        Rectangle rect = new Rectangle("Green", .08, .09);
         shapes.Add(rect);
-        Circle circle = new Circle();
+        Circle circle = new Circle("Purple", 90);
         shapes.Add(circle);
         // TODO: add one of each shape (Square, Rectangle, Circle)
         // each should include color and size values
@@ -22,6 +22,12 @@ class Program
         // TODO: loop through the list
         // for each shape, display the color and computed area
         // use GetColor() and GetArea()
+        //oh like the polymorph video
+        foreach (Shape shape in shapes)
+        {
+            shape.GetColor();
+            shape.GetArea();
+        }
 
         // TODO: test output to verify polymorphism works correctly
         // (each shape type should calculate its own area)
