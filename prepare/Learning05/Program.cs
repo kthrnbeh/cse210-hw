ce4009156fc0e90d8fdce0f2dfcd91fa5a509849
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-14 13:05:19 -0600

    updates

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic; // for the list of shapes
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        // TODO: create a list to hold shapes (List<Shape>)
+        // example: List<Shape> shapes = new List<Shape>();
+
+        // TODO: add one of each shape (Square, Rectangle, Circle)
+        // each should include color and size values
+
+        // TODO: loop through the list
+        // for each shape, display the color and computed area
+        // use GetColor() and GetArea()
+
+        // TODO: test output to verify polymorphism works correctly
+        // (each shape type should calculate its own area)
     }
 }
