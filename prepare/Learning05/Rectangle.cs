57b0c9068c1ebbd95c8535b333297004631da978
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-14 13:03:51 -0600

    update

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Rectangle : Shape // derived class from Shape
 {
     private double _length; // from diagram (_length : double)
@@ -7,11 +9,14 @@ public class Rectangle : Shape // derived class from Shape
     {
         // TODO: set both _length and _width from the parameters
         // this constructor should call the base class constructor for color
+        _length = length;
+        _width = width;
     }
 
     public override double GetArea()
     {
         // TODO: calculate and return the area of a rectangle
         // formula: length * width
+        return _length * _width;
     }
 }
