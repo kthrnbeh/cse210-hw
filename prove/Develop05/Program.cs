66228b4471bb33b55f725e0991df14c69b5d314a
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-15 10:56:37 -0600

    upate

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,63 +6,64 @@ class Program
     {
         Console.WriteLine("Hello Develop05 World!");
         //first step is to make a menu
-    //Taking ideas from Develope 2 for the menu
-    int menuNumber = 0;
-    while (menuNumber != 4)
-    {// found this to guide https://video.byui.edu/media/t/1_f4017z1d/259941952
-      // my idea is to write a menu and have it keep going until it gets the number 5
-      Console.WriteLine("Menu Options:");
-      Console.WriteLine("1.Start Breathing Activity");
-      Console.WriteLine("2.Start Reflecting Activity");
-      Console.WriteLine("3.Start Listening Activity");
-      Console.WriteLine("4.Quit");
+        //Taking ideas from Develope 2 for the menu
+        int menuNumber = 0;
+        while (menuNumber != 4)
+        {// found this to guide https://video.byui.edu/media/t/1_f4017z1d/259941952
+         // my idea is to write a menu and have it keep going until it gets the number 5
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1.Start Breathing Activity");
+            Console.WriteLine("2.Start Reflecting Activity");
+            Console.WriteLine("3.Start Listening Activity");
+            Console.WriteLine("4.Quit");
 
-      Console.Write("Select a choice from the Menu ");
+            Console.Write("Select a choice from the Menu ");
 
-      string userInput = Console.ReadLine();
-      if (!int.TryParse(userInput, out menuNumber))
+            string userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out menuNumber))
             {
                 Console.WriteLine("Invalid input. Please enter a number.");
-                Thread.Sleep(2000); 
+                Thread.Sleep(2000);
                 continue;
             }
 
-       if (menuNumber == 1)
-        {
-       
-
-        //what can I call
-
-        }
-       else if (menuNumber == 2)
-        {
-             
-        
-        
-        }
-       else if (menuNumber == 3)
-        {
-        
-     
+            if (menuNumber == 1)
+            {
 
 
-         }
-       else if (menuNumber == 4)
-        {
+                //what can I call
+
+            }
+            else if (menuNumber == 2)
+            {
+
+
+
+            }
+            else if (menuNumber == 3)
+            {
+
+
+
+
+            }
+            else if (menuNumber == 4)
+            {
                 Console.WriteLine("Quit selected. Goodbye!");
                 Thread.Sleep(1000);
                 break;
-        }
-        else
-        {
+            }
+            else
+            {
                 Console.WriteLine("Invalid Choice");
                 Thread.Sleep(1000);
 
+            }
+
         }
-      
+
     }
-      
-    }
+    //https://video.byui.edu/media/t/1_6lpgm7dz
     
   
 
