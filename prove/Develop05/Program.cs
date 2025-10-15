a018271d8069636e3e3ab040e811b29f74c08363
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-15 16:27:23 -0600

    update

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -9,16 +9,18 @@ class Program
         //create a goal manager object
         //call the start funciton on that object
         //create a list of goal objects
-       // List<Goal> goals = new List<Goal>();
-       // SimpleClass simple = new SimpleClass();
+        // List<Goal> goals = new List<Goal>();
+        // SimpleClass simple = new SimpleClass();
         //goals.Add(simple);
         //Goal goal = goals[0];
         Console.WriteLine("Hello Develop05 World!");
+        GoalManager manager = new GoalManager();
         
+     
 
     }
     //https://video.byui.edu/media/t/1_6lpgm7dz
-    
+    //https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/program-structure/main-command-line
   
 
 }
