0be22b41eff99c552c48935711d604c1db30b40e
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-15 13:42:45 -0600

    updates

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,34 +1,38 @@
-// // Purpose: Never completes; awards points every time.
+// Purpose: Never completes; awards points every time.
+public class EternalGoal : Goal
+{
+    // CONSTRUCTOR
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    {
+        // TODO: any eternal-specific init if needed
 
-// public class EternalGoal : Goal
-// {
-//     // CONSTRUCTOR
-//     public EternalGoal(string name, string description, int points) : base(name, description, points)
-//     {
-//         // TODO: any eternal-specific init if needed
-//     }
+    }
 
-//     // OVERRIDES (names exactly as diagram)
-//     public override void RecordEvent()
-//     {
-//         // TODO: award base points each time; never completes
-//     }
+    // OVERRIDES (names exactly as diagram)
+    public override int RecordEvent()
+    {
+        // TODO: award base points each time; never completes
+        Console.WriteLine($"{_points}");
+        return _points; //the amount of points each time
+    }
 
-//     public override bool isComplete()
-//     {
-//         // TODO: always false
-//         return false; // placeholder comment-only skeleton; remove on implement
-//     }
+    public override bool IsComplete()
+    {
+        // TODO: always false
+        return false;
+    }
 
-//     public override string GetstringRepresent()
-//     {
-//         // TODO: return "Eternal|name|description|points"
-//         return ""; // placeholder comment-only skeleton; remove on implement
-//     }
+    public override string GetstringRepresent()
+    {
+        // TODO: return "Eternal|name|description|points"
+       return $"[]Eternal Goal|{_name}|{_description}|{_points}";
+    }
 
-//     public override string GetDetails()
-//     {
-//         // TODO: return "[ ] Name — Description (+points each time)"
-//         return ""; // placeholder comment-only skeleton; remove on implement
-//     }
-// }
+    public override string GetDetails()
+    {
+        // TODO: return "[ ] Name — Description (+points
+        return $"[x]Eternal Goal|{_name}|{_description}|{_points}";
+            
+    }
+}
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/abstract
