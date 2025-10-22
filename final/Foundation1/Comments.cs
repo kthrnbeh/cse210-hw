d54e687c36ef796532f1c47396bca8b1c97ba256
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 09:53:10 -0600

    updates

--- final/Foundation1/Comments.cs
+++ final/Foundation1/Comments.cs
@@ -11,7 +11,8 @@ namespace YouTubeVideos
         // TODO: Create a constructor that sets both fields when a new comment is made.
     public Comment(string name, string text)
         {
-            
+            _name = name;
+            _text = text;
         }
         // TODO: (Optional) Create public getters or a method to display the comment details.
     }
