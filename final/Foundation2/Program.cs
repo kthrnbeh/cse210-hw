25a36f45b406ea687ec230d0152250cacbd9a935
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 10:29:50 -0600

    what I will be ref

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,11 @@
 using System;
 
 class Program
-{
+{//Encapsulation Part 2
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
     }
 }
+//https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html
+//https://byui-cse.github.io/cse210-course-2023/unit03/prepare.html
