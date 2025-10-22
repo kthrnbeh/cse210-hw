ad33fb394cead14e14007d22b06d7e74feaf5396
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 13:57:31 -0600

    updates

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -5,6 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+          // TODO: Create at least two Address objects (USA and non-USA).
+            // TODO: Create at least two Customer objects using those addresses.
+            // TODO: Create 2–3 Product objects per order with names, ids, prices, quantities.
+            // TODO: Create at least two Order objects with different customers.
+            // TODO: Add products to each order via AddProduct().
+            // TODO: For each order, display:
+            //       - Packing Label (GetPackingLabel())
+            //       - Shipping Label (GetShippingLabel())
+            //       - Total Price (GetTotalCost())
+            
     }
 }
 //https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html
