1faf7fc2f3f992087434fcc4d2fa619d53c97316
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 13:54:48 -0600

    updates

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -4,3 +4,21 @@ The name is a string, but the Address is a class.
 The customer should have a method that can return whether 
 they live in the USA or not. (Hint this should call a method on the address to find this.)
 */
+public class Customer
+{
+    private string _name;
+    private int _address;
+    public Customer(string name, int address)
+    {
+        _name = name;
+        _address = address;
+    }
+    public string GetName()
+    {
+        return _name;
+    }
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+}
