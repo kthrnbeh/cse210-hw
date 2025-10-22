65c46d92f2620b1b4b5fcc372a0825cffff66d88
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 13:40:48 -0600

    updates

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,8 +1,11 @@
 /*
 Contains a list of products and a customer. Can calculate the total cost of the order. 
 Can return a string for the packing label. Can return a string for the shipping label.
-The total price is calculated as the sum of the total cost of each product plus a one-time shipping cost.
-This company is based in the USA. If the customer lives in the USA, then the shipping cost is $5. If the customer does not live in the USA, then the shipping cost is $35.
+The total price is calculated as the sum of the total cost of each product plus 
+a one-time shipping cost.
+This company is based in the USA. 
+If the customer lives in the USA, then the shipping cost is $5. 
+If the customer does not live in the USA, then the shipping cost is $35.
 A packing label should list the name and product id of each product in the order.
 A shipping label should list the name and address of the customer
 */
@@ -10,6 +13,7 @@ public class Order //so glad I did so much reading yesterday and today on encaps
 {
     private List<Product> _products = new List<Product>();
     private string _customer;
+    private string _address;
     public Order(List<Product> products, string customer)
     {
         _products = products;
@@ -36,8 +40,19 @@ public class Order //so glad I did so much reading yesterday and today on encaps
         double totalCost = 0;
         foreach (Product product in _products)
         {
-            totalCost += procuct.GetTotalCost();
+            totalCost += product.GetTotalCost();
         }
+        double shippingCost = 0;
+        if(_customer.Contains("USA"))//could use IndexOf instead but decided its better in case its lowercase
+        {
+            shippingCost = 5;
+        }
+        else
+        {
+            shippingCost = 35;
+        }
+        return totalCost + shippingCost; 
     }
+
     
-}
+}//https://www.dotnetperls.com/contains
