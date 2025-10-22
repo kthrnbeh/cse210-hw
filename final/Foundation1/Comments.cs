1f933c69e59c8ea1dfcd1c0d5435ec099f92e4b2
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 09:52:44 -0600

    updates

--- final/Foundation1/Comments.cs
+++ final/Foundation1/Comments.cs
@@ -6,9 +6,13 @@ namespace YouTubeVideos
     public class Comment
     {
         // TODO: Create private fields to store the name of the person and the text of the comment.
-
+        private string _name;
+        private string _text;
         // TODO: Create a constructor that sets both fields when a new comment is made.
-
+    public Comment(string name, string text)
+        {
+            
+        }
         // TODO: (Optional) Create public getters or a method to display the comment details.
     }
 }
