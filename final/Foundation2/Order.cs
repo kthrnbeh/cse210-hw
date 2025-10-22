04825bea030f87c9a54e80491d7fec348468385b
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 13:26:34 -0600

    updates

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,5 +1,6 @@
 /*
-Contains a list of products and a customer. Can calculate the total cost of the order. Can return a string for the packing label. Can return a string for the shipping label.
+Contains a list of products and a customer. Can calculate the total cost of the order. 
+Can return a string for the packing label. Can return a string for the shipping label.
 The total price is calculated as the sum of the total cost of each product plus a one-time shipping cost.
 This company is based in the USA. If the customer lives in the USA, then the shipping cost is $5. If the customer does not live in the USA, then the shipping cost is $35.
 A packing label should list the name and product id of each product in the order.
@@ -29,6 +30,14 @@ public class Order //so glad I did so much reading yesterday and today on encaps
     public void SetCustomer(string customer)
     {
         _customer = customer;
+    }
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach (Product product in _products)
+        {
+            totalCost += procuct.GetTotalCost();
+        }
     }
     
 }
