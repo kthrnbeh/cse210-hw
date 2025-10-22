d63b9b59a428c6ab93b2f285c071268e7b31dd17
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 10:31:31 -0600

    updates

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -9,3 +9,4 @@ class Program
 }
 //https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html
 //https://byui-cse.github.io/cse210-course-2023/unit03/prepare.html
+//https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/
