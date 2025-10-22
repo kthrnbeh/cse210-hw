298e9cb59f1a6a1eec84a3acb4e56889a9ea97f5
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 10:36:37 -0600

    updates

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,5 @@
+/*
+Contains the name, product id, price per unit, and quantity of each product.
+The total cost of this product is computed by multiplying the price per unit and the quantity. 
+If the price per unit was $3 and they bought 5, the product total cost would be $15.
+*/
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -10,3 +10,9 @@ class Program
 //https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html
 //https://byui-cse.github.io/cse210-course-2023/unit03/prepare.html
 //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/
+/*
+Make sure that all member variables are private and getters, setters, and constructors are created as needed.
+
+Once you have created these classes, write a program that creates at least two orders with a 2-3 products each. Call the methods to get the packing label, the shipping label, 
+and the total price of the order, and display the results of these methods.
+*/
