debe8c5dfd26845b6c360ea4cbb9a6cbb4677d01
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-22 17:44:08 -0600

    updates

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -13,8 +13,6 @@ public class Order // so glad I did so much reading yesterday and today on encap
 {
     private List<Product> _products = new List<Product>();
     private Customer _customer;
-    private string _address;
-
     public Order(Customer customer)
     {
         _customer = customer;
@@ -64,7 +62,7 @@ public class Order // so glad I did so much reading yesterday and today on encap
     {
         string shippingLabel = "Shipping To: \n";
         shippingLabel += $"{_customer.GetName()}"; // ok so call the customer and the method to get the name 
-        shippingLabel += $"{_customer.GetAddress()}";// call to get the address
+        shippingLabel += $"{_customer.GetAddress().GetFullAddress()}";// call to get the address
         return shippingLabel;
     }
 }
