627d4a42f3f3df48e1a0a155dd4a03598d0c34fa
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 12:30:23 -0600

    updates

--- final/Foundation3/Lectures.cs
+++ final/Foundation3/Lectures.cs
@@ -1,4 +1,10 @@
 public class Lectures : Events
 {
-    
+    private string _speakerName;
+    private int _capacity;
+    public Lectures(string speakerName, int capacity)base:()
+    {
+        _speakerName = speakerName;
+        _capacity = capacity;
+    }
 }
