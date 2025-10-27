b2e42e274d2fb7d32ea0ad376cf21139117b3aa1
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 12:31:54 -0600

    maybe

--- final/Foundation3/Lectures.cs
+++ final/Foundation3/Lectures.cs
@@ -2,7 +2,7 @@ public class Lectures : Events
 {
     private string _speakerName;
     private int _capacity;
-    public Lectures(string speakerName, int capacity)base:()
+    public Lectures(string _title, string _description, DateTime _date, Timespan _time,string speakerName, int capacity)base:(string _title, string _description, DateTime _date, Timespan _time,)
     {
         _speakerName = speakerName;
         _capacity = capacity;
