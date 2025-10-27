732e29d2b65ba148f6220afcee3bacd639f521b8
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 13:00:36 -0600

    update

--- final/Foundation3/Outdoor.cs
+++ final/Foundation3/Outdoor.cs
@@ -1,8 +1,17 @@
+//Outdoor gatherings, which do not have a limit on attendees, but need to track the weather forecast
 public class Outdoor : Events
 {
-    
-    public Outdoor()
+    private string _weatherForcast;
+    public Outdoor(string title, string description, DateTime date, TimeSpan time, Address address, string weatherForcast) : base(title, description, date, time, address)
     {
-
+        _weatherForcast = weatherForcast;
+    }
+    public string GetWeatherForcast()
+    {
+        return _weatherForcast;
+    }
+    public void SetWeatherForCast(string weatherForcast)
+    {
+        _weatherForcast = weatherForcast;
     }
 }
