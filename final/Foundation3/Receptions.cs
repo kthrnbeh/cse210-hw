861d5a414a51a32fa85b276762f6c96c544d44ef
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 12:54:05 -0600

    updates

--- final/Foundation3/Receptions.cs
+++ final/Foundation3/Receptions.cs
@@ -1,7 +1,19 @@
-/*Write a program that has a base Event class along with derived classes for each type of event. These classes should contain the necessary data and provide methods to return strings for each of the messages the company desires.
-Remember that any data or methods that are common among all types of events should be in the base class.
-Once you have the classes in place, write a program that creates at least one event of each type and sets all of their values. Then, for event event, call each of the methods to generate the marketing messages and output their results to the screen.
-In addition, your program must:
-Use inheritance to avoid duplicating shared attributes and methods.
-Use an address class for the addresses.
-Follow the principles of encapsulation, making sure each member variable is private.*/
+public class Receptions : Events
+{
+    private string _RSVP;
+
+    public Receptions(string RSVP,string title, string description, DateTime date, TimeSpan time,Address address):base(title,description,date,time,address)
+    {
+        _RSVP = RSVP;
+
+    }
+    public string GetRSVP()
+    {
+        return _RSVP;
+    }
+    public void SetRSVP(string RSVP)
+    {
+        _RSVP = RSVP;
+    }
+
+}
