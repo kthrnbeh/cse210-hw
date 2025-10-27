2ba9b5de9f1ad9fe15e3b022c3e5da7c48a173cf
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 12:42:15 -0600

    so stressful

--- final/Foundation3/Lectures.cs
+++ final/Foundation3/Lectures.cs
@@ -2,9 +2,25 @@ public class Lectures : Events
 {
     private string _speakerName;
     private int _capacity;
-    public Lectures(string _title, string _description, DateTime _date, Timespan _time,string speakerName, int capacity)base:(string _title, string _description, DateTime _date, Timespan _time,)
+    public Lectures(string title, string description, DateTime date, TimeSpan time, string address, string speakerName, int capacity) : base(title, description, date, time, address)
     {
         _speakerName = speakerName;
+        _capacity = capacity;
+    }
+    public string GetSpeakerName()
+    {
+        return _speakerName;
+    }
+    public void SetSpeakerName(string speakerName)
+    {
+        _speakerName = speakerName;
+    }
+    public int GetCapacity()
+    {
+        return _capacity;
+    }
+    public void SetCapacity(int capacity)
+    {
         _capacity = capacity;
     }
 }
