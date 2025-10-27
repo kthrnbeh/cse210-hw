a3041dfea07d18d5edb2ed466ebd57970e7ff66b
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 08:53:06 -0600

    updates

--- final/Foundation3/Events.cs
+++ final/Foundation3/Events.cs
@@ -5,5 +5,12 @@ public class Events
     protected DateTime _date;
     protected TimeSpan _time;
     protected int _address;
-    
+
+    public Events(string title, string description, DateTime date, TimeSpan time, int address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _address = address;
+    }
 }
