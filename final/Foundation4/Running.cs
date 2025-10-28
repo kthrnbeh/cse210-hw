0f7bcc623d211b4215e0205a41f88efd3922cdf4
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 18:42:47 -0600

    updates

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -5,8 +5,24 @@ public class Running : Activity
     {
         _distance = distance;
     }
-    public double GetDistance()
+    public double GetDistance()//I really know how to do this part now. 
     {
         return _distance;
+    }
+    public void SetDistance(double distance)
+    {
+        _distance = distance;
+    }
+    public override double GetDistance()
+    {
+        return 0;
+    }
+    public override double GetPace()
+    {
+        return 0;
+    }
+    public override double GetSpeed()
+    {
+        return 0;
     }
 }
