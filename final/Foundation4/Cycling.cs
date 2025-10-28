51b286a769d2b9f191417a99b6f76acef02eb4ad
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 18:31:01 -0600

    don't forget

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -3,11 +3,11 @@ public class Cycling : Activity
     private double _avgSpeed;
     public Cycling(DateTime date, int minutes, double avgSpeed) : base(date, minutes)
     {
-        _avgSpeed = avgSpeed
+        _avgSpeed = avgSpeed;
     }
     public double GetAvgSpeedMPH()
     {
-        return _avgSpeed
+        return _avgSpeed;
     }
     public void SetAvgSpeedMPH(double avgSpeed)
     {
