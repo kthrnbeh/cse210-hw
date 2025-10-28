c3a038c6863b90e19d4bb54e217a9848df4f2087
kthrnbeh <kthrnbeh@yahoo.com>
2025-10-27 18:07:29 -0600

    updates

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,11 @@
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _time;
+    public Activity(DateTime date, int time)
+    {
+        _date = date;
+        _time = time;
+    }
+    
+}
